1ad172c9d839814265e2052fbba4787b2af580e5
SantiSeeds <santi.weber@weareseeders.com>
2021-06-13 18:35:52 -0300

    secure is secure

--- src/communication/endpoints/EndpointList.js
+++ src/communication/endpoints/EndpointList.js
@@ -1,3 +1,3 @@
-export const userListEndpoint = "http://tdp2-tp1-users-service.herokuapp.com/user/"
+export const userListEndpoint = "https://tdp2-tp1-users-service.herokuapp.com/user/"
 export const backofficeToken = '7objfFTJUoAcHoLGJ2VT2Q'
 export const reportsEndpoint = "https://tdp2-tp1-users-service.herokuapp.com/report/"
